d6cdddc8752bc3abe4b162fbad4d09913027041f
ShaderOX <obsidianic18k@gmail.com>
2020-12-08 15:33:35 +0500

    Add google seach support and better API response

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-from typing import Text
+from typing import Dict, Text, List
 from flask import Flask
 from flask_restful import Resource, Api
 from keywords.keywords import extract
+from keywords.api import get_google_results
 
 app = Flask(__name__)
 api = Api(app)
@@ -10,10 +11,21 @@ api = Api(app)
 class TextToLinks(Resource):
 
     def get(self: Resource, text: str):
-        return {
-            "links": extract(text)
-        }
+        results: List[Dict[str, str]] = []
 
+        phrases = extract(text)
+
+        query = ""
+        for phrase in phrases:
+            query += phrase
+
+        for result in get_google_results(query):
+            results.append({
+                'title': result.title,
+                'link': result.link
+            })
+
+        return results
 
 
 api.add_resource(TextToLinks, '/<string:text>')
