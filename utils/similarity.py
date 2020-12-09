70ce61c6b6a31c7a6f05646fc477cac126ef8ff6
green-mint <saad.521509@gmail.com>
2020-12-09 13:55:06 +0500

    migrated similarity.py from pytorh and sbert to spacy

--- utils/similarity.py
+++ utils/similarity.py
@@ -1,18 +1,15 @@
-from sentence_transformers import SentenceTransformer, util
+import spacy
 from typing import List
 
 
 def similarity(tweet: str, article_heads: List[str]) -> List[float]:
     """Returns the similarity indices for each of the titles"""
 
-    model = SentenceTransformer('distilbert-base-nli-stsb-mean-tokens')
+    model = spacy.load('en_core_web_md')
     cosine_scores: List[float] = []
-    tweet_embedding = model.encode(tweet, convert_to_tensor=True)
-
+    tweet_embedding = model(tweet)
     for i in range(len(article_heads)):
-        article_embedding = model.encode(
-            article_heads[i], convert_to_tensor=True)
-        cosine_scores.append(float(util.pytorch_cos_sim(
-            tweet_embedding, article_embedding).numpy()))
+        article_embedding = model(article_heads[i])
+        cosine_scores.append(tweet_embedding.similarity(article_embedding))
 
-    return cosine_scores
+    return cosine_scores 
